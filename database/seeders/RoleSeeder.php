97c73f0225c810457e2c9f7902d50488957056ca
awad3317 <awadaaa3317@gmail.com>
2025-01-10 13:50:34 +0300

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -20,10 +20,10 @@ class RoleSeeder extends Seeder
         ]);
         $permissions = Permission::all()->pluck('id')->toArray();
         $adminRole->permissions()->attach($permissions);
-        
+
         Role::create(
             ['name'=>'user',
             'display_name'=>'مستخدم']
-        )->permissions()->attach([9,10,11]);
+        )->permissions()->attach([9,10,11,21,25,27,28,29,30]);
     }
 }
