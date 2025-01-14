d710db72014b939b918d0d8acaf0078db2229312
awad3317 <awadaaa3317@gmail.com>
2025-01-14 23:00:55 +0300

    update auth-form.blade.php

--- resources/views/forget_password.blade.php
+++ resources/views/forget_password.blade.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite(['resources/sass/app.scss','resources/js/app.js'])
-    <link rel="stylesheet" href="{{asset('assets/css/custom-style.css')}}">
-</head>
+@extends('layouts.head')
+@section('title' ,'إعادة تعيين كلمة المرور')
 <body>
     <div class="row">
         <div class="col-md-5 d-none d-lg-flex row ">
