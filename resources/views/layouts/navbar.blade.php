21913979290cfdff53cf3b3e29c495a74f9f34e6
awad3317 <awadaaa3317@gmail.com>
2025-01-21 21:03:56 +0300

    update navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light shadow" dir="rtl">
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow" dir="rtl" id="navbar">
     <div class="container-fluid">
         <div style="width: 150px; height: 100px;">
-            <img alt="" class="mt-3 mx-lg-3 mx-1" style="max-width: 150px; max-height: 200px; height:auto"  src="{{asset('assets/images/icon.png')}}">
+            <img alt="" class="mt-3 mx-lg-3 mx-1" style="max-width: 150px; max-height: 200px; height:auto" src="{{asset('assets/images/icon.png')}}">
         </div>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,32 +13,32 @@
                 <ul class="navbar-nav d-inline-flex">
                     <li>
                         @if(Auth::user()->name)
-                    <div class="d-lg-none text-center mb-2">
-                        <a class="link  fw-bold text-decoration-none" href="{{route('account_show')}}" style="font-size: 20px">{{Auth::user()->name}}</a>
-                    </div>
-                    @else
-                    <span class="mx-4">
-                        <button class="btn btn-primary border" style="font-size: 20px">تسجيل الدخول</button>
-                    </span>
-                    @endif
+                            <div class="d-lg-none text-center mb-2">
+                                <a class="link fw-bold text-decoration-none" href="{{route('account_show')}}" style="font-size: 20px">{{Auth::user()->name}}</a>
+                            </div>
+                        @else
+                            <span class="mx-4">
+                                <button class="btn btn-primary border" style="font-size: 20px">تسجيل الدخول</button>
+                            </span>
+                        @endif
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link fs-5 mx-2 active" aria-current="page" href="#">الرئيسية</a>
+                        <a class="nav-link fs-5 mx-2 @if(request()->is('home')) active-bg @endif" aria-current="page" href="{{ url('/home') }}">الرئيسية</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link fs-5 mx-2" href="#">من نحن</a>
+                        <a class="nav-link fs-5 mx-2 @if(request()->is('about')) active-bg @endif" href="{{ url('about') }}">من نحن</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link fs-5 mx-2" href="#">اتصل بنا</a>
+                        <a class="nav-link fs-5 mx-2 @if(request()->is('contact')) active-bg @endif" href="{{ url('contact') }}">اتصل بنا</a>
                     </li>
                 </ul>
             </div>
         </div>
+
         @if(Auth::user()->name)
             <span class="d-none d-lg-flex align-items-center mx-4">
                 <a class="link fw-bold text-decoration-none p-0 mx-2" href="{{route('account_show')}}" style="border: none; background: none; font-size: 20px">{{Auth::user()->name}}</a>
                 <img class="ms-2" src="{{asset('assets/images/person.png')}}" alt="" style="width: 82px; height: 82px; border-radius: 50%;">
-                
             </span>
         @else
             <span class="mx-4">
