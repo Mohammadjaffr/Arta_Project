ba1c3377fa009cb3edef036ba99b08158824f28f
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-01-27 11:40:13 +0300

    add new ads page

--- resources/views/livewire/image-uploader.blade.php
+++ resources/views/livewire/image-uploader.blade.php
@@ -1,22 +1,45 @@
 <div>
-    <div class="row justify-content-center my-4">
-        <div class="col-12 col-md-8 border border-dark rounded-5 p-3" style="background-color: rgba(1, 73, 107, 0.68);">
+    <div class="d-flex justify-content-center my-4">
+        <div class="col-12 col-md-8 rounded-5 p-3" style="background-color: #046998C9;width: 100%;height: 100%;">
             <div class="text-end">
-                <h6>رفع الصورة</h6>
-                <div>
-                    <label class="px-3">حدد الصورة الأساسية</label>
-                    <!-- الصورة الأساسية -->
-                    <img id="mainImagePreview" class="border border-amber-600 rounded-2" style="width: 70px; height: 70px; cursor: pointer;" src="{{ $mainImage ? $mainImage->temporaryUrl() : asset('assets/images/facebook.svg') }}" alt="صورة أساسية">
+                <h6 style="font-size: large">رفع الصورة</h6>
+                <div class="border border-dark rounded-3" style="width: 100px; height: 80px; position: relative;background-color: #C3C3C3;">
+                    <!-- الصورة الأساسية إذا كانت موجودة -->
+                    @if(isset($mainImage))
+                        <img id="mainImagePreview" class="" style="width: 100px; height: 80px; cursor: pointer;"
+                             src="{{ $mainImage->temporaryUrl() }}" alt="صورة أساسية"
+                             onclick="document.getElementById('fileInput').click();">
+                    @else
+                        <img id="mainImagePreview" class="" style="width: 50px; height: 50px; cursor: pointer;margin: 10% 22%;"
+                             src="{{ asset('assets/images/add-photo.png') }}" alt="صورة افتراضية"
+                             onclick="document.getElementById('fileInput').click();">
+                    @endif
+
                     <!-- حقل رفع الملف المخفي -->
-                    <input wire:model="mainImage" type="file" accept="image/*" style="display: none;">
+                    <input id="fileInput" wire:model="mainImage" type="file" accept="image/*" style="display: none;"
+                           onchange="previewImage(event)">
+                    
                 </div>
             </div>
             <div class="d-flex flex-wrap justify-content-evenly mt-3">
                 <!-- مربعات رفع الصور -->
                 @for ($i = 0; $i < 4; $i++)
-                    <div class="text-center">
-                        <img class="border rounded-3 image-preview" style="width: 120px; height: 100px; background-color: #C3C3C3; cursor: pointer;" src="{{ isset($images[$i]) ? $images[$i]->temporaryUrl() : asset('assets/images/add-photo.png') }}" alt="إضافة صورة">
-                        <input wire:model="images.{{ $i }}" type="file" accept="image/*" style="display: none;">
+                    <div class="text-center" style="width: 120px; height: 100px;">
+                        <!-- عرض الصورة المرفوعة -->
+                        @if (isset($images[$i]) && $images[$i])
+                            <img class="border rounded-3 image-preview" style="width: 120px; height: 100px; object-fit: cover;" src="{{ $images[$i]->temporaryUrl() }}" alt="صورة مرفوعة">
+                        @else
+                            <!-- مربع فارغ مع أيقونة إضافة الصور في المربع الأول فقط -->
+                            <div class="border rounded-3" style="width: 120px; height: 100px; background-color: #C3C3C3; cursor: pointer;" onclick="document.getElementById('image-input-{{ $i }}').click();">
+                                @if ($i === 0)
+                                    <!-- أيقونة إضافة الصور (تظهر فقط في المربع الأول) -->
+                                    <img  src="{{ asset('assets/images/add-photo.png') }}" alt="إضافة صورة" style="width: 70px; height: 70px;margin-top: 10%; ">
+                                @endif
+                            </div>
+                        @endif
+
+                        <!-- حقل رفع الملف -->
+                        <input wire:model="images.{{ $i }}" type="file" accept="image/*" style="display: none;" id="image-input-{{ $i }}">
                     </div>
                 @endfor
             </div>
@@ -25,10 +48,19 @@
 </div>
 
 <script>
-    // إضافة حدث النقر للصورة الأساسية
-    document.getElementById('mainImagePreview').addEventListener('click', function () {
-        this.nextElementSibling.click(); // فتح حقل رفع الملف
-    });
+    function previewImage(event) {
+        const input = event.target;
+        const file = input.files[0];
+        const preview = document.getElementById('mainImagePreview');
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                preview.src = e.target.result;
+            }
+            reader.readAsDataURL(file);
+        }
+    }
 
     // إضافة أحداث لمربعات رفع الصور
     document.querySelectorAll('.image-preview').forEach((preview, index) => {
