bc28172920b5079e0c0930e7d24b2543698e2a6d
awad3317 <awadaaa3317@gmail.com>
2025-02-01 20:39:26 +0300

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -39,26 +39,44 @@
 
         @if(Auth::user())
 
-            <div class="dropdown ">
-                <a class="btn btn-light mx-4 px-5 text-white " style="background-color: #01496B" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {{Auth::user()->name}}
+        <div class="nav-item dropdown no-arrow ms-4 d-none d-lg-flex">
+            <a class="btn" 
+               style="background-color: transparent; border: 2px solid #0056b3; color: #007BFF; border-radius: 5px; padding: 5px 10px;" 
+               href="#" 
+               role="button" 
+               data-bs-toggle="dropdown" 
+               aria-expanded="false">
+                <i class="fas fa-user">{{ Auth::user()->name }}</i>
+            </a>
+            
+            <!-- Dropdown - User Information -->
+            <div class="dropdown-menu dropdown-menu-right shadow-lg animated--grow-in" 
+                 aria-labelledby="userDropdown" style="text-align: start;">
+                <div class="dropdown-item" style="display: flex; flex-direction: column; align-items: center; background-color: rgba(248, 249, 250, 0.8); padding: 10px; border-radius: 5px; background-image: url('{{ asset('assets/images/icon.png') }}'); background-repeat: repeat; background-size: 20px 20px;">
+                    <img src="{{asset('assets/images/person.png')}}" alt="User Image" 
+                         style="width: 50px; height: 50px; border-radius: 50%; margin-top: 5px;">
+                </div>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" 
+                   href="#" 
+                   data-toggle="modal" 
+                   data-target="#logoutModal" 
+                   style="transition: background-color 0.3s;">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    تسجيل خروج
                 </a>
-
-                <ul class="dropdown-menu ">
-                    <li><a class="dropdown-item" href="{{route('account_show')}}">
-                            <img class="ms-2" src="{{asset('assets/images/person.png')}}" alt="" style="width: 40px; height: 40px; border-radius: 50%;">
-                                <span>البروفايل</span>
-                        </a>
-                    </li>
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i
-                            class="bx bx-log-out"></i> تسجيل خروج</a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </ul>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" 
+                   href="{{route('account_show')}}" 
+                   style="transition: background-color 0.3s;">
+                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    ملفي الشخصي
+                </a>
             </div>
-
+        </div>
+        
+        
+        
         @else
             <span class="d-none d-lg-flex align-items-center mx-4">
                 <a href="{{route('login')}}">
