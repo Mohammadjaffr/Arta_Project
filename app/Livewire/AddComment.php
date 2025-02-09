b72e5d68e340a66f190fdd7d5ae381d6cc7b3fca
Mohammad_jaffer_alsaadi <Mohammadjaffrsaadix@gmail.com>
2025-02-09 20:57:48 +0300

    add component comment

---
+++ app/Livewire/AddComment.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Comment;
+use App\Models\listing;
+use App\Repositories\CommentRepository;
+use App\Repositories\ListingRepository;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class AddComment extends Component
+{
+
+    public $content;
+    public $listingId;
+    private ListingRepository $listingRepository;
+    public function mount($listingId,ListingRepository $listingRepository)
+    {
+        $this->listingId = $listingId;
+        $this->listingRepository = $listingRepository;
+    }
+
+    public function addComment()
+    {
+        if (empty($this->content)) {
+            session()->flash('error', 'يرجى كتابة تعليق قبل الإرسال.');
+
+        }
+
+        Comment::create([
+            'user_id' => Auth::id(),
+            'listing_id' => $this->listingId,
+            'content' => $this->content,
+        ]);
+
+        $this->reset('content');
+
+        // رسالة نجاح
+        session()->flash('message', 'تم إضافة التعليق بنجاح.');
+        return redirect()->to('/show_info/' . $this->listingId);
+    }
+
+    public function render()
+    {   $listing = $this->listingRepository->getById($this->listingId);
+        return view('livewire.add-comment', compact('listing'));
+    }
+}
