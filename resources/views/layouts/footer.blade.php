7f9cac9b51887bd8736606a890a5a07428952432
awad3317 <awadaaa3317@gmail.com>
2025-02-10 14:16:35 +0300

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,20 +3,20 @@
         <div class="row m-0 justify-content-between align-items-center">
             <div class="col-12 col-md-6 d-flex justify-content-center justify-content-md-start mb-3 mb-md-4">
                 <button class="rounded-circle me-2 social-button border-0">
-                    <img src="{{asset('assets/images/facebook.svg')}}" alt="Facebook">
+                    <img src="{{asset('assets/images/facebook.svg')}}" alt="Facebook" loading="lazy">
                 </button>
                 <button class="rounded-circle me-2 social-button border-0">
-                    <img src="{{asset('assets/images/twitter.svg')}}" alt="Twitter">
+                    <img src="{{asset('assets/images/twitter.svg')}}" alt="Twitter" loading="lazy">
                 </button>
                 <button class="rounded-circle me-2 social-button border-0">
-                    <img src="{{asset('assets/images/instagram.svg')}}" alt="Instagram">
+                    <img src="{{asset('assets/images/instagram.svg')}}" alt="Instagram" loading="lazy">
                 </button>
                 <button class="rounded-circle social-button border-0">
-                    <img style="width: 25px;" src="{{asset('assets/images/whatsapp.svg')}}" alt="WhatsApp">
+                    <img style="width: 25px;" src="{{asset('assets/images/whatsapp.svg')}}" alt="WhatsApp" loading="lazy">
                 </button>
             </div>
             <div class="col-12 col-md-6 d-flex flex-column align-items-md-end align-items-center justify-content-between">
-                <img alt="icon" style="max-width: 150px; max-height: 200px; height:auto" src="{{asset('assets/images/icon.png')}}">
+                <img alt="icon" style="max-width: 150px; max-height: 200px; height:auto" src="{{asset('assets/images/icon.png')}}" loading="lazy">
                 <h3 class="me-4 fw-bold" style="color: var(--primary-custom-color);font-family: 'Tajawal', sans-serif;" >منصة عرطة</h3>
             </div>
         </div>
@@ -24,10 +24,10 @@
             <h4 class="">استكشف التطبيق الخاص بنا</h4>
             <div class="col-12 col-md-4">
                 <button style="border: none; background-color: white;">
-                    <img style="width: 150px;" src="{{asset('assets/images/appstore.png')}}" alt="App Store">
+                    <img style="width: 150px;" src="{{asset('assets/images/appstore.png')}}" alt="App Store" loading="lazy">
                 </button>
                 <button style="border: none; background-color: white;">
-                    <img style="width: 150px;" src="{{asset('assets/images/googlestore.png')}}" alt="Google Store">
+                    <img style="width: 150px;" src="{{asset('assets/images/googlestore.png')}}" alt="Google Store" loading="lazy">
                 </button>
             </div>
             <div class="col-12 col-md-4 py-3">
