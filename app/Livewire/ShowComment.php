c91ead4f8fa9fbacd571e7aca3ac5a97a1e584b5
awad3317 <awadaaa3317@gmail.com>
2025-02-12 13:22:31 +0300

    Update ShowComment.php

--- app/Livewire/ShowComment.php
+++ app/Livewire/ShowComment.php
@@ -5,17 +5,14 @@ namespace App\Livewire;
 use App\Models\Comment;
 use Livewire\Component;
 use Livewire\Attributes\On;
-use App\Repositories\CommentRepository;
 
 class ShowComment extends Component
 {
     public $listingId;
     public $commentsToShow = 4;
-    private $CommentRepository;
     public function mount($listingId)
     {
         $this->listingId = $listingId;
-        $this->CommentRepository=new CommentRepository();
     }
     public function loadMoreComments()
     {
