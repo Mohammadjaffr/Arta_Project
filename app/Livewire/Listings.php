22b15cb34dafef435ed91201b09580f0b802b9a5
awad3317 <awadaaa3317@gmail.com>
2025-02-14 19:18:53 +0300

    add filter by region

--- app/Livewire/Listings.php
+++ app/Livewire/Listings.php
@@ -11,7 +11,9 @@ class Listings extends Component
 {
     #[Url()]
     public $title;
-    public $Parent_id;
+    public $region_parent_id;
+
+    public $region_child_id;
     public function mount()
     {
         Carbon::setLocale('ar');
@@ -25,18 +27,39 @@ class Listings extends Component
     public function render()
     {
         $query = Listing::query();
-        $Parents=Region::whereNull('parent_id')->get();;
-        if(isset($this->Parent_id)){
-            $region = Region::with('children')->find($this->Parent_id);
+
+        $childrens = $this->region_parent_id
+        ? Region::where('parent_id', $this->region_parent_id)->get()
+        : null;
+
+        if ($this->region_child_id and $this->region_parent_id) {
+            
+            $query->where('region_id', $this->region_child_id);
+        }elseif ($this->region_parent_id) {
+            $this->region_child_id=null;
+            $region = Region::with('children')->find($this->region_parent_id);
             if ($region) {
                 $regionIds = $region->children()->pluck('id')->push($region->id);
                 $allRegionIds = Region::whereIn('parent_id', $regionIds)->pluck('id');
                 $regionIds = $regionIds->merge($allRegionIds);
                 $query->whereIn('region_id', $regionIds);
-            } 
+            }
         }
-        $listings = $query->where('title','like',"%{$this->title}%")->with(['user:id,name','category:id,name','region:id,name','images','comments.user','currency:id,code,name,abbr'])->paginate(10);
+        if ($this->title) {
+            $query->where('title', 'like', "%{$this->title}%");
+        }
+        $listings = $query->with([
+            'user:id,name',
+            'category:id,name',
+            'region:id,name',
+            'images',
+            'comments.user',
+            'currency:id,code,name,abbr'
+        ])->paginate(10);
+
+        $Parents = Region::whereNull('parent_id')->get();
+        
         Carbon::setLocale('ar');
-        return view('livewire.listings',compact('listings','Parents'));
+        return view('livewire.listings',compact('listings','Parents','childrens'));
     }
 }
